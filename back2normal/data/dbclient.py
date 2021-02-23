0bd1778197968c8dc571cc05305c59d9b0333d07
Christine Ibaraki <>
2021-02-22 22:14:10 -0600

    replace string format with f, minor cleanup

--- back2normal/data/dbclient.py
+++ back2normal/data/dbclient.py
@@ -7,18 +7,18 @@ PRAGMA_INFO_DTYPE_INDEX = 2
 
 class DBClient:
 
-    def __init__(self, db_path = DB_PATH):
+    def __init__(self, db_path=DB_PATH):
 
         self.conn = sqlite3.connect(db_path)
         # is it ok to persist this?
         self.cursor = self.conn.cursor()
 
-    def create_table_from_pandas(self, data_df, table_name, replace = True):
+    def create_table_from_pandas(self, data_df, table_name, replace=True):
 
         data_df.to_sql(table_name, self.conn, if_exists='replace' if replace else 'fail')
 
     def get_table_info(self, table_name):
 
-        pragma_statement = "pragma table_info({})".format(table_name)
+        pragma_statement = f"pragma table_info({table_name})"
         return [(x[PRAGMA_INFO_FIELD_INDEX], x[PRAGMA_INFO_DTYPE_INDEX])
                 for x in self.cursor.execute(pragma_statement).fetchall()]
