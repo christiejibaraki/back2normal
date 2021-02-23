bf523658b5c22372f113565a6a7381e6773c6614
ColeVFrank <cole.v.frank@gmail.com>
2021-02-22 19:17:25 -0500

    made review_db a cript

--- back2normal/review_db_dtypes_script.py
+++ back2normal/review_db_dtypes_script.py
@@ -4,20 +4,13 @@ from util import basic_io
 
 APP_TOKEN_STR = "app_token"
 
+path_to_keys = os.path.join('config', 'socrata_chicago_keys.json')
+keys = basic_io.read_json_to_dict(path_to_keys)
+params={"$$app_token": keys[APP_TOKEN_STR]}
 
-def main():
+response_obj = api_requests.RequestResponse("https://data.cityofchicago.org/resource/naz8-j4nc.json", params)
 
-    path_to_keys = os.path.join('config', 'socrata_chicago_keys.json')
-    keys = basic_io.read_json_to_dict(path_to_keys)
-    params={"$$app_token": keys[APP_TOKEN_STR]}
-
-    response_obj = api_requests.RequestResponse("https://data.cityofchicago.org/resource/naz8-j4nc.json", params)
-
-    table_name = "test_table"
-    db = dbclient.DBClient()
-    db.create_table_from_pandas(response_obj.data_df, table_name)
-    print(db.get_table_info(table_name))
-
-
-if __name__ == "__main__":
-    main()
+table_name = "test_table"
+db = dbclient.DBClient()
+db.create_table_from_pandas(response_obj.data_df, table_name)
+print(db.get_table_info(table_name))
