d6a48fa169caa2d5166f678ecb323b12a96f1b0b
Christine Ibaraki <>
2021-02-22 21:13:25 -0600

    implemented select query, small change to update pandas dtype, modified review script

--- back2normal/review_db_dtypes_script.py
+++ back2normal/review_db_dtypes_script.py
@@ -10,8 +10,17 @@ params={"$$app_token": keys[APP_TOKEN_STR]}
 
 response_obj = api_requests.RequestResponse("https://data.cityofchicago.org/resource/naz8-j4nc.json", params)
 response_obj.convert_types() #converts dtypes
+print(response_obj.df_dtypes)
 
 table_name = "test_table"
 db = dbclient.DBClient()
 db.create_table_from_pandas(response_obj.data_df, table_name)
 print(db.get_table_info(table_name))
+
+#### select specific fields
+response_obj_fields = api_requests.RequestResponse(
+    "https://data.cityofchicago.org/resource/naz8-j4nc.json",
+    params, ["cases_total", "deaths_total"])
+print(response_obj_fields.df_dtypes)
+response_obj_fields.convert_types() #converts dtypes
+print(response_obj_fields.df_dtypes)
