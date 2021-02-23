34f9fcafe70859fdf05ad30d8cef501c88bb1954
Christine Ibaraki <>
2021-02-22 22:27:37 -0600

    some cleanup. removed the request session, we can add it back if requests are slow. used response.request to get the url

--- back2normal/data/api_requests.py
+++ back2normal/data/api_requests.py
@@ -1,17 +1,12 @@
-import os
 import requests
-import sqlite3
 import pandas as pd
-from util import basic_io
-import sys
 
 
 class RequestResponse:
 
     def __init__(self, request_url, params, select_field_list = None):
 
-        self._session = requests.Session()
-        self.request = None
+        self.request_url = None
         self.response = None
         self.data_df = None
 
@@ -23,8 +18,7 @@ class RequestResponse:
         self._get_request(request_url, params, select_field_list)
 
     def _get_request(self, request_url, params, select_field_list):
-        # need to implement query
-        # https://dev.socrata.com/docs/queries/
+        # socrata query: https://dev.socrata.com/docs/queries/
 
         # For header codes included in response object: 
         # https://dev.socrata.com/docs/response-codes.html
@@ -33,26 +27,20 @@ class RequestResponse:
         # https://requests.readthedocs.io/en/master/user/advanced/#request-and-response-objects
         # ()
 
+        # if select_field_list not empty or None, create select query
         if not not select_field_list:
             select_statement = f"?$select={', '.join(select_field_list)}"
             request_url = request_url + select_statement
-       
-        self.request = requests.Request(request_url, params)
-        self.response = self._session.get(request_url, params = params)
-        #self.request = self.response.request
 
-        self.data_df = pd.DataFrame.from_dict(self.response.json())
-        self.df_dtypes = self.data_df.dtypes
-
+        # get and parse response
+        self.response = requests.get(request_url, params = params)
+        self.request_url = self.response.request.url  # siiick this is exactly what i wanted, thanks
         self.header_fields = self.response.headers['X-SODA2-Fields']
         self.header_dtypes = self.response.headers['X-SODA2-Types']
 
-    def print_request(self):
-        # doesn't work, but for debugging queries
-        print('{}{}{}'.format(
-            self.request.method, self.request.url,
-            ''.join('{}={}'.format(k, v) for k, v in self.request.headers.items())
-        ))
+        # convert to pandas df
+        self.data_df = pd.DataFrame.from_dict(self.response.json())
+        self.df_dtypes = self.data_df.dtypes
 
     def convert_types(self):
         # making this a method that's not called by constructor for now
@@ -61,4 +49,3 @@ class RequestResponse:
         self.data_df = self.data_df.apply(pd.to_numeric, errors='ignore')
         self.data_df = self.data_df.convert_dtypes(convert_integer=False)
         self.df_dtypes = self.data_df.dtypes
-
--- back2normal/review_db_dtypes_script.py
+++ back2normal/review_db_dtypes_script.py
@@ -6,10 +6,11 @@ APP_TOKEN_STR = "app_token"
 
 path_to_keys = os.path.join('config', 'socrata_chicago_keys.json')
 keys = basic_io.read_json_to_dict(path_to_keys)
-params={"$$app_token": keys[APP_TOKEN_STR]}
+params = {"$$app_token": keys[APP_TOKEN_STR]}
 
 response_obj = api_requests.RequestResponse("https://data.cityofchicago.org/resource/naz8-j4nc.json", params)
 response_obj.convert_types() #converts dtypes
+print(response_obj.request_url)
 print(response_obj.df_dtypes)
 
 table_name = "test_table"
@@ -21,6 +22,7 @@ print(db.get_table_info(table_name))
 response_obj_fields = api_requests.RequestResponse(
     "https://data.cityofchicago.org/resource/naz8-j4nc.json",
     params, ["cases_total", "deaths_total"])
+print(response_obj_fields.request_url)
 print(response_obj_fields.df_dtypes)
 response_obj_fields.convert_types() #converts dtypes
 print(response_obj_fields.df_dtypes)
