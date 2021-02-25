c14681c44061c1a0266766c0fb09dccfeb4565fb
Christine Ibaraki <>
2021-02-24 23:02:37 -0600

    update docstrings

--- back2normal/data/data_transformations.py
+++ back2normal/data/data_transformations.py
@@ -15,6 +15,8 @@ WEEK_END_TO_CDC_WEEK = basic_io.read_json_to_dict("data/cdc_week.json")
 
 def get_zipcode_from_mapbox(long, lat, access_token):
     """
+    get zipcode for geo coords via mapbox api
+
     :param long: longitude
     :param lat: latitude
     :param access_token: mapbox api access token
@@ -29,17 +31,25 @@ def get_zipcode_from_mapbox(long, lat, access_token):
 
 def get_next_saturday(YYYY_MM_DD_str):
     """
+    get date obj representing nearest saturday AFTER input date
+
     :param YYYY_MM_DD_str: (str) date in format 'YYYY-MM-DD'
     :return: datetime object representing the next saturday
     """
     date_obj = datetime.strptime(YYYY_MM_DD_str, SOCRATA_STR_DT_FORMAT)
     date_obj.weekday()
-    t = timedelta((SATURDAY_INDEX - date_obj.weekday()) % DAYS_IN_WEEK)
-    return date_obj + t
+    days_until_saturday = timedelta((SATURDAY_INDEX - date_obj.weekday()) % DAYS_IN_WEEK)
+    return date_obj + days_until_saturday
 
 
 def get_cdc_mmwr_week(YYY_MM_DD_str):
+    """
+    get CDC MMWR week for input date string (weeks are Sun-Sat)
+    See https://ibis.health.state.nm.us/resource/MMWRWeekCalendar.html#part3
 
+    :param YYY_MM_DD_str: (str) date
+    :return: CDC week number
+    """
     next_saturday = get_next_saturday(YYY_MM_DD_str)
     next_sat_str = next_saturday.strftime(SOCRATA_STR_DT_FORMAT)
     return WEEK_END_TO_CDC_WEEK[next_sat_str]
