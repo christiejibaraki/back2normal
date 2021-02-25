a36562176753bb9449f03ac8219b170034a3a458
Christine Ibaraki <>
2021-02-25 13:22:46 -0600

    add test for cdc week lookup

--- back2normal/tests/data/test_data_transformations.py
+++ back2normal/tests/data/test_data_transformations.py
@@ -31,3 +31,17 @@ def test_get_cdc_mmr_week():
     assert data_transformations.get_cdc_mmwr_week(yyyymmdd_str) == 18
 
 
+def test_cdc_lookup_resource():
+    week_ending_to_cdc_week_dict = basic_io.read_json_to_dict("data/cdc_week.json")
+
+    assert week_ending_to_cdc_week_dict["2020-05-02"] == 18
+    assert week_ending_to_cdc_week_dict["2023-04-15"] == 15
+    assert week_ending_to_cdc_week_dict["2024-12-28"] == 52
+    assert week_ending_to_cdc_week_dict["2026-01-03"] == 53
+    assert week_ending_to_cdc_week_dict["2022-01-01"] == 52
+    assert week_ending_to_cdc_week_dict["2016-01-09"] == 1
+    assert week_ending_to_cdc_week_dict["2018-04-07"] == 14
+    assert week_ending_to_cdc_week_dict["2021-01-02"] == 53
+
+
+
