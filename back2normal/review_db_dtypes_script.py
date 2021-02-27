12f6e6639ed5d2badbc75db77108fa6fbf5cc4f0
ColeVFrank <cole.v.frank@gmail.com>
2021-02-27 15:51:53 -0500

    cleaned up the review script to reflect refactored classes and added a couple test queries. The last one which includes a group by clause and a where clause is not working. think its a problem with the where clause and will come back to this later today

--- back2normal/review_db_dtypes_script.py
+++ back2normal/review_db_dtypes_script.py
@@ -4,13 +4,9 @@ from util import basic_io
 
 APP_TOKEN_STR = "app_token"
 
-
-
-
 path_to_keys = os.path.join('config', 'socrata_chicago_keys.json')
 keys = basic_io.read_json_to_dict(path_to_keys)
 params = {"$$app_token": keys[APP_TOKEN_STR]}
-query = ['*']
 
 response_obj = api_requests.SocrataAPIClient("https://data.cityofchicago.org/resource/naz8-j4nc.json", params)
 response_obj.convert_types() #converts dtypes
@@ -21,28 +17,6 @@ table_name = "test_table"
 db = dbclient.DBClient()
 db.create_table_from_pandas(response_obj.data_df, table_name)
 print(db.get_table_info(table_name))
-
-#### select specific fields
-# response_obj_fields = api_requests.SocrataAPIClient(
-#     "https://data.cityofchicago.org/resource/naz8-j4nc.json",
-#     params)
-    
-# print(response_obj_fields.request_url)
-# print(response_obj_fields.df_dtypes)
-# response_obj_fields.convert_types() #converts dtypes
-# print(response_obj_fields.df_dtypes)
-
-#### group by some attribute
-#### i picked an arbitrary dataset that would be good for testing this
-# response_obj_slct_group = api_requests.SocrataAPIClient(
-#     "https://data.cityofchicago.org/resource/mq3i-nnqe.json",
-#     params, ["on_street", "COUNT(stop_id)"], ["on_street"])
-# print(response_obj_slct_group.request_url)
-# print(response_obj_slct_group.header_fields)
-# print(response_obj_slct_group.df_dtypes)
-# response_obj_slct_group.convert_types() #converts dtypes
-# print(response_obj_slct_group.df_dtypes)
-
 
 
 #### add datasets from dict
@@ -55,3 +29,42 @@ for data_obj in soda_data.datasets:
     api_resp.convert_types()
     db.create_table_from_pandas(api_resp.data_df, data_obj.table_name)
     print(db.get_table_info(data_obj.table_name))
+
+
+#Testing group by but no where clause
+soda_obj_groupby_query = soda_data.SodaData("Traffic Crashes - Crashes",
+                         "TRAFFIC_CRASHES",
+                         "85ca-t3if",
+                         ["COUNT(CRASH_RECORD_ID)", "CRASH_DATE"],
+                         group_by=['CRASH_DATE'])
+
+print(soda_obj_groupby_query.request_url)
+
+api_resp_groupby = api_requests.SocrataAPIClient(soda_obj_groupby_query.request_url,
+                                                    params)
+print(api_resp_groupby.request_url)
+print(api_resp_groupby.header_fields)
+print(api_resp_groupby.data_df)
+
+
+
+#Testing more complex query with WHERE and GROUP BY fields
+#This query is not working--and i think the WHERE clause
+#is the problem
+# CRASH_DATE is a Floating Timestamp Datatype
+    # https://dev.socrata.com/docs/datatypes/floating_timestamp.html#,
+soda_obj_complex_query = soda_data.SodaData("Traffic Crashes - Crashes",
+                         "TRAFFIC_CRASHES",
+                         "85ca-t3if",
+                         ["COUNT(CRASH_RECORD_ID)", "CRASH_DATE"],
+                         group_by=['CRASH_DATE'],
+                         where=["CRASH_DATE > 2020-01-01T14:00:00"])
+print(soda_obj_complex_query.request_url)
+
+api_resp_complex = api_requests.SocrataAPIClient(soda_obj_complex_query.request_url,
+                                             params)
+print(api_resp_complex.request_url)
+print(api_resp_complex.header_fields)
+print(api_resp_complex.data_df)
+
+
