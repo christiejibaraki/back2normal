825663df773e3ec9f2f7c08e3578350da3401baa
Shai Slotky <shaislotky@Shais-MBP.attlocal.net>
2021-02-28 22:51:03 -0600

    debugged groundtruth foottraffic dataframe generator

--- back2normal/data/groundtruth/process_ground_truth_data.py
+++ back2normal/data/groundtruth/process_ground_truth_data.py
@@ -1,19 +1,22 @@
 import os
+import pandas as pd
 from util import basic_io
 
-list_of_desired_cols = ['Bars']
-
 ground_truth_file_path = os.path.join("resources", "GroundTruth")
-for dirName, subdirList, fileList in os.walk(ground_truth_file_path):
-    for fname in fileList:
-        data = basic_io.read_csv_to_list(os.path.join(ground_truth_file_path, fname))
-        header = data.pop(0)
-        header_2 = data.pop(0)
-        header[0] = header_2[0]# add 'Timestamp Date' to header
-        print(fname)
-        print(header)
+df_list = []
 
-#combine files 
-#if some files do not contain the same column then they should say none
-#combine into single pandas dataframe 
-#create additional column for zipcode 
+for dirName, subdirList, fileList in os.walk(ground_truth_file_path):
+    fileList.remove(".DS_Store")
+    for fname in fileList:
+        df = pd.read_csv("resources/GroundTruth/" + fname)
+        #remove index line
+        df = df.drop([0])
+        #create time stamp header
+        df.rename(columns = {"Category": "Time Stamp"})
+        #create zip code column + header
+        df.insert(1, "ZIP Code", fname.split(".")[0])
+        #append to dataframe list
+        df_list.append(df)
+
+#merged groundtruth dataframe
+gt_df = pd.concat(df_list)
