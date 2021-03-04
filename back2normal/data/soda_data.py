ceb784194c5884b973e8851909f22454ea0c6caa
Christine Ibaraki <>
2021-03-03 21:04:35 -0600

    add field for list of cols on which weekly avg will be computed

--- back2normal/data/soda_data.py
+++ back2normal/data/soda_data.py
@@ -1,29 +1,41 @@
 
 class SodaData:
 
-    def __init__(self, dataset_name, table_name, identifier,
+    def __init__(self, dataset_name, sql_table_name, identifier,
                  desired_attr_lst,
+                 week_avg_attr_lst=None,
                  group_by=None,
                  where=None,
                  limit=None):
 
         self.dataset_name = dataset_name
-        self.table_name = table_name
+        self.sql_table_name = sql_table_name
         self.identifier = identifier
+
+        # constructing api query
         self.base_url = f"https://data.cityofchicago.org/resource/{identifier}.json"
         self.desired_attr_lst = desired_attr_lst
         self.group_by_lst = group_by
         self.where_lst = where
         self.limit = limit
+
+        # api request url
         self.request_url = self._build_soql_query()
 
+        # variables for which weekly averages will be computed
+        self.week_avg_attr_list = week_avg_attr_lst
+
     def _build_soql_query(self):
+        """
+        builds a soql query to append to base request url.
+
+        :return: (str) socrata api request url
+        """
 
         # soql docs: https://dev.socrata.com/docs/queries/
         
         query =  f"?$query=SELECT {', '.join(self.desired_attr_lst)} "
         if self.where_lst:
-            #Multiple clauses AND
             query += f"WHERE {'AND '.join(self.where_lst)} "
         if self.group_by_lst:
             query += f"GROUP BY {', '.join(self.group_by_lst)}"
@@ -32,10 +44,8 @@ class SodaData:
 
         return self.base_url + query
 
-        #         request_url = request_url + group_statement
 
-
-datasets = []
+datasets = {}
 
 ############################################
 ######## COVID-19 Vaccinations by ZIP Code
@@ -43,14 +53,16 @@ datasets = []
 
 # https://dev.socrata.com/foundry/data.cityofchicago.org/553k-3xzc
 
-datasets.append(SodaData("COVID-19 Vaccinations by ZIP Code",
-                         "VACCINATIONS_DAILY",
-                         "553k-3xzc",
-                         ["zip_code", "date",
-                          "total_doses_daily", "total_doses_cumulative",
-                          "vaccine_series_completed_daily",
-                          "vaccine_series_completed_percent_population",
-                          "population"],
-                         None,
-                         None,
-                         5000))
+datasets["COVID-19 Vaccinations by ZIP Code"] = \
+    SodaData("COVID-19 Vaccinations by ZIP Code",
+             "VACCINATIONS_DAILY",
+             "553k-3xzc",
+             ["zip_code", "date",
+              "total_doses_daily", "total_doses_cumulative",
+              "vaccine_series_completed_daily",
+              "vaccine_series_completed_percent_population",
+              "population"],
+             ["total_doses_daily", "vaccine_series_completed_daily"],
+             None,
+             None,
+             5000)
