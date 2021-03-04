cf2dcb6f34c0a2c7fed892a3c91a52c3e50ca771
Christine Ibaraki <>
2021-03-03 21:20:31 -0600

    refactored into a function

--- back2normal/data/groundtruth/process_ground_truth_data.py
+++ back2normal/data/groundtruth/process_ground_truth_data.py
@@ -1,22 +1,24 @@
 import os
 import pandas as pd
-from util import basic_io
 
-ground_truth_file_path = os.path.join("resources", "GroundTruth")
-df_list = []
+GROUND_TRUTH_FILE_PATH = os.path.join("resources", "GroundTruth")
 
-for dirName, subdirList, fileList in os.walk(ground_truth_file_path):
-    fileList.remove(".DS_Store")
-    for fname in fileList:
-        df = pd.read_csv("resources/GroundTruth/" + fname)
-        #remove index line
-        df = df.drop([0])
-        #create time stamp header
-        df.rename(columns = {"Category": "Time Stamp"})
-        #create zip code column + header
-        df.insert(1, "ZIP Code", fname.split(".")[0])
-        #append to dataframe list
-        df_list.append(df)
 
-#merged groundtruth dataframe
-gt_df = pd.concat(df_list)
+def get_combined_ground_truth_data():
+    df_list = []
+
+    for dirName, subdirList, fileList in os.walk(GROUND_TRUTH_FILE_PATH):
+        for fname in fileList:
+            if '.csv' in fname:
+                df = pd.read_csv(os.path.join(GROUND_TRUTH_FILE_PATH, fname))
+                # remove index line
+                df = df.drop([0])
+                # create time stamp header
+                df.rename(columns={"Category": "Time Stamp"}, inplace=True)
+                # create zip code column + header
+                df.insert(1, "ZIP Code", fname.split(".")[0])
+                # append to dataframe list
+                df_list.append(df)
+
+    # merged groundtruth dataframe
+    return pd.concat(df_list)
