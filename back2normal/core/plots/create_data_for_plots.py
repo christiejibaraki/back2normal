e6399e56e727606ac929adf6b38fa461b44e0967
ColeVFrank <cole.v.frank@gmail.com>
2021-03-13 23:02:49 -0500

    changing demographic_data.js format

--- back2normal/core/plots/create_data_for_plots.py
+++ back2normal/core/plots/create_data_for_plots.py
@@ -8,7 +8,13 @@ def get_demographic_data(output_file):
     db = dbclient.DBClient()
     query = f"select * from {build_db.CENSUS_TBL}"
     census_df = pd.read_sql_query(query, db.conn)
-    demographic_data = census_df.to_dict(orient="records")
+    demographic_data = []
+    for i, zipc in enumerate(census_df.ZIPCODE):
+        for cat in census_df.columns:
+            demographic_data.append({"ZIPCODE": zipc, "CATEGORY": cat, 
+                                     "VALUE": census_df[cat][i]})
     basic_io.write_dict_to_json(output_file, demographic_data)
 
 get_demographic_data("demographic_data.js")
+
+
