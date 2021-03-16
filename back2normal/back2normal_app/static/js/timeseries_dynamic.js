c50249e82f938bb4f5c6b86cf1a76d271571c71e
Christine Ibaraki <>
2021-03-16 10:58:12 -0500

    dynmic series plot

---
+++ back2normal/back2normal_app/static/js/timeseries_dynamic.js
@@ -0,0 +1,1010 @@
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    width = 860 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3
+    .select("#dynamic_series")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// append the svg object to the body of the page
+var svg2 = svg
+    .select("#cvg_svgC2")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + +")");
+
+var svg3 = svg
+    .append("svg")
+    .attr("id", "cvg_svgC3")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + +")");
+
+var svg4 = svg
+    .append("svg")
+    .attr("id", "cvg_svgC4")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + +")");
+
+var data = [
+    {
+        date: "2013-04-28",
+        value: 135.98
+    },
+    {
+        date: "2013-04-29",
+        value: 147.49
+    },
+    {
+        date: "2013-04-30",
+        value: 146.93
+    },
+    {
+        date: "2013-05-28",
+        value: 130.58
+    },
+    {
+        date: "2013-05-29",
+        value: 132.59
+    },
+    {
+        date: "2013-05-30",
+        value: 132.25
+    },
+    {
+        date: "2013-05-31",
+        value: 129.9
+    },
+    {
+        date: "2013-06-01",
+        value: 129.78
+    },
+    {
+        date: "2013-06-02",
+        value: 129.4
+    },
+    {
+        date: "2013-06-03",
+        value: 122.5
+    },
+    {
+        date: "2013-06-04",
+        value: 123.84
+    },
+    {
+        date: "2013-06-05",
+        value: 123.47
+    },
+    {
+        date: "2013-06-06",
+        value: 123.1
+    },
+    {
+        date: "2013-06-07",
+        value: 119
+    },
+    {
+        date: "2013-06-08",
+        value: 111.42
+    },
+    {
+        date: "2013-06-09",
+        value: 108.99
+    },
+    {
+        date: "2013-06-10",
+        value: 110.1
+    },
+    {
+        date: "2013-06-11",
+        value: 109.6
+    },
+    {
+        date: "2013-07-02",
+        value: 92.3
+    },
+    {
+        date: "2013-07-03",
+        value: 90.98
+    },
+    {
+        date: "2013-07-04",
+        value: 83.11
+    },
+    {
+        date: "2013-07-05",
+        value: 80
+    },
+    {
+        date: "2013-07-06",
+        value: 75
+    },
+    {
+        date: "2013-07-07",
+        value: 74.56
+    },
+    {
+        date: "2013-07-08",
+        value: 80
+    },
+    {
+        date: "2013-07-09",
+        value: 78.3
+    },
+    {
+        date: "2013-07-10",
+        value: 87
+    },
+    {
+        date: "2013-07-11",
+        value: 90.28
+    },
+    {
+        date: "2013-07-12",
+        value: 104
+    },
+    {
+        date: "2013-07-13",
+        value: 98.25
+    },
+    {
+        date: "2013-07-14",
+        value: 98.7
+    },
+    {
+        date: "2013-07-15",
+        value: 101.9
+    },
+    {
+        date: "2013-07-16",
+        value: 99.86
+    },
+    {
+        date: "2013-07-17",
+        value: 99.97
+    },
+    {
+        date: "2013-07-18",
+        value: 98.8
+    },
+    {
+        date: "2013-07-19",
+        value: 95.2
+    },
+    {
+        date: "2013-07-20",
+        value: 93.1
+    },
+    {
+        date: "2013-07-21",
+        value: 91.95
+    },
+    {
+        date: "2013-07-22",
+        value: 92
+    },
+    {
+        date: "2013-07-23",
+        value: 96.82
+    },
+    {
+        date: "2013-07-24",
+        value: 95.99
+    },
+    {
+        date: "2013-07-25",
+        value: 97.33
+    },
+    {
+        date: "2013-07-26",
+        value: 97.47
+    },
+    {
+        date: "2013-07-27",
+        value: 97
+    },
+    {
+        date: "2013-07-28",
+        value: 100.58
+    },
+    {
+        date: "2013-07-29",
+        value: 102.5
+    },
+    {
+        date: "2013-07-30",
+        value: 107.99
+    },
+    {
+        date: "2013-07-31",
+        value: 111.34
+    },
+    {
+        date: "2013-08-01",
+        value: 108
+    },
+    {
+        date: "2013-08-02",
+        value: 108
+    },
+    {
+        date: "2013-08-03",
+        value: 105.78
+    },
+    {
+        date: "2013-08-04",
+        value: 105.95
+    },
+    {
+        date: "2013-08-05",
+        value: 107.77
+    },
+    {
+        date: "2013-08-06",
+        value: 107.38
+    },
+    {
+        date: "2013-08-07",
+        value: 106.75
+    },
+    {
+        date: "2013-08-08",
+        value: 106.75
+    },
+    {
+        date: "2013-08-09",
+        value: 105.75
+    },
+    {
+        date: "2013-08-10",
+        value: 103.9
+    },
+    {
+        date: "2013-08-11",
+        value: 105.19
+    },
+    {
+        date: "2013-08-12",
+        value: 108
+    },
+    {
+        date: "2013-08-13",
+        value: 109.35
+    },
+    {
+        date: "2013-08-14",
+        value: 115
+    },
+    {
+        date: "2013-08-15",
+        value: 113.25
+    },
+    {
+        date: "2013-08-16",
+        value: 112.3
+    },
+    {
+        date: "2013-08-17",
+        value: 113.75
+    },
+    {
+        date: "2013-08-18",
+        value: 114.69
+    },
+    {
+        date: "2013-08-19",
+        value: 123.06
+    },
+    {
+        date: "2013-08-20",
+        value: 123.01
+    },
+    {
+        date: "2013-08-21",
+        value: 124.91
+    },
+    {
+        date: "2013-08-22",
+        value: 123.5
+    },
+    {
+        date: "2013-08-23",
+        value: 122
+    },
+    {
+        date: "2013-08-24",
+        value: 121.39
+    },
+    {
+        date: "2013-08-25",
+        value: 122.99
+    },
+    {
+        date: "2013-08-26",
+        value: 122.75
+    },
+    {
+        date: "2013-08-27",
+        value: 127.24
+    },
+    {
+        date: "2013-08-28",
+        value: 127.32
+    },
+    {
+        date: "2013-08-29",
+        value: 123.7
+    },
+    {
+        date: "2013-08-30",
+        value: 135.75
+    },
+    {
+        date: "2013-08-31",
+        value: 140.89
+    },
+    {
+        date: "2013-09-01",
+        value: 145.81
+    },
+    {
+        date: "2013-09-02",
+        value: 146.5
+    },
+    {
+        date: "2013-09-03",
+        value: 138.34
+    },
+    {
+        date: "2013-09-04",
+        value: 144.5
+    },
+    {
+        date: "2013-09-05",
+        value: 131.44
+    },
+    {
+        date: "2013-09-06",
+        value: 127.34
+    },
+    {
+        date: "2013-09-07",
+        value: 125.94
+    },
+    {
+        date: "2013-09-08",
+        value: 124.72
+    },
+    {
+        date: "2013-09-09",
+        value: 129.06
+    },
+    {
+        date: "2013-09-10",
+        value: 129.74
+    },
+    {
+        date: "2013-09-11",
+        value: 137.83
+    },
+    {
+        date: "2013-09-12",
+        value: 136.08
+    },
+    {
+        date: "2013-09-13",
+        value: 137.58
+    },
+    {
+        date: "2013-09-14",
+        value: 135.85
+    },
+    {
+        date: "2013-09-15",
+        value: 131.44
+    },
+    {
+        date: "2013-09-16",
+        value: 132.72
+    },
+    {
+        date: "2013-09-17",
+        value: 132.76
+    },
+    {
+        date: "2013-09-18",
+        value: 133.2
+    },
+    {
+        date: "2013-09-19",
+        value: 131.77
+    },
+    {
+        date: "2013-09-20",
+        value: 135.62
+    },
+    {
+        date: "2013-09-21",
+        value: 128.61
+    },
+    {
+        date: "2013-09-22",
+        value: 133.94
+    },
+    {
+        date: "2013-09-23",
+        value: 132.72
+    },
+    {
+        date: "2013-09-24",
+        value: 127.46
+    },
+    {
+        date: "2013-09-25",
+        value: 129.69
+    },
+    {
+        date: "2013-09-26",
+        value: 134.93
+    },
+    {
+        date: "2013-09-27",
+        value: 134.74
+    },
+    {
+        date: "2013-09-28",
+        value: 135.63
+    },
+    {
+        date: "2013-09-29",
+        value: 140.61
+    },
+    {
+        date: "2013-09-30",
+        value: 138.35
+    },
+    {
+        date: "2013-10-01",
+        value: 134.63
+    },
+    {
+        date: "2013-10-02",
+        value: 133.59
+    },
+    {
+        date: "2013-10-03",
+        value: 123.63
+    },
+    {
+        date: "2013-10-04",
+        value: 130.09
+    },
+    {
+        date: "2013-10-05",
+        value: 130.44
+    },
+    {
+        date: "2013-10-06",
+        value: 129.66
+    },
+    {
+        date: "2013-10-07",
+        value: 130.27
+    },
+    {
+        date: "2013-10-08",
+        value: 127.47
+    },
+    {
+        date: "2013-10-09",
+        value: 131.75
+    },
+    {
+        date: "2013-10-10",
+        value: 131.5
+    },
+    {
+        date: "2013-10-11",
+        value: 131.81
+    },
+    {
+        date: "2013-10-12",
+        value: 135.66
+    },
+    {
+        date: "2013-10-13",
+        value: 138.66
+    },
+    {
+        date: "2013-10-14",
+        value: 144.12
+    },
+    {
+        date: "2013-10-15",
+        value: 145.39
+    },
+    {
+        date: "2013-10-16",
+        value: 152.23
+    },
+    {
+        date: "2013-10-17",
+        value: 147.42
+    },
+    {
+        date: "2013-10-18",
+        value: 155.96
+    },
+    {
+        date: "2013-10-19",
+        value: 177.48
+    },
+    {
+        date: "2013-10-20",
+        value: 174.91
+    },
+    {
+        date: "2013-10-21",
+        value: 184.82
+    },
+    {
+        date: "2013-10-22",
+        value: 196.27
+    },
+    {
+        date: "2013-10-23",
+        value: 213.62
+    },
+    {
+        date: "2013-10-24",
+        value: 217.42
+    },
+    {
+        date: "2013-10-25",
+        value: 198.87
+    },
+    {
+        date: "2013-10-26",
+        value: 188.41
+    },
+    {
+        date: "2013-10-27",
+        value: 196.44
+    },
+    {
+        date: "2013-10-28",
+        value: 198.62
+    },
+    {
+        date: "2013-10-29",
+        value: 204.79
+    },
+    {
+        date: "2013-10-30",
+        value: 209.16
+    },
+    {
+        date: "2013-10-31",
+        value: 205.18
+    },
+    {
+        date: "2013-11-01",
+        value: 206.65
+    },
+    {
+        date: "2013-11-02",
+        value: 207.76
+    },
+    {
+        date: "2013-11-03",
+        value: 215.05
+    },
+    {
+        date: "2013-11-04",
+        value: 231.01
+    },
+    {
+        date: "2013-11-05",
+        value: 250.66
+    },
+    {
+        date: "2013-11-06",
+        value: 263
+    },
+    {
+        date: "2013-11-07",
+        value: 304.17
+    },
+    {
+        date: "2013-11-08",
+        value: 338.66
+    },
+    {
+        date: "2013-11-09",
+        value: 370.82
+    },
+    {
+        date: "2013-11-10",
+        value: 350.7
+    },
+    {
+        date: "2013-11-11",
+        value: 351.27
+    },
+    {
+        date: "2013-11-12",
+        value: 362.81
+    },
+    {
+        date: "2013-11-13",
+        value: 414.05
+    },
+    {
+        date: "2013-11-14",
+        value: 425.9
+    },
+    {
+        date: "2013-11-15",
+        value: 437.89
+    },
+    {
+        date: "2013-11-16",
+        value: 450.26
+    },
+    {
+        date: "2013-11-17",
+        value: 500.58
+    },
+    {
+        date: "2013-11-18",
+        value: 703.78
+    },
+    {
+        date: "2013-11-19",
+        value: 806.11
+    },
+    {
+        date: "2013-11-20",
+        value: 599.65
+    },
+    {
+        date: "2013-11-21",
+        value: 733.4
+    },
+    {
+        date: "2013-11-22",
+        value: 780.85
+    },
+    {
+        date: "2013-11-23",
+        value: 844.97
+    },
+    {
+        date: "2013-11-24",
+        value: 807.36
+    },
+    {
+        date: "2013-11-25",
+        value: 810.68
+    },
+    {
+        date: "2013-11-26",
+        value: 928.54
+    },
+    {
+        date: "2013-11-27",
+        value: 1001.96
+    },
+    {
+        date: "2013-11-28",
+        value: 1077.56
+    },
+    {
+        date: "2013-11-29",
+        value: 1146.97
+    },
+    {
+        date: "2013-11-30",
+        value: 1156.14
+    },
+    {
+        date: "2013-12-01",
+        value: 1133.08
+    },
+    {
+        date: "2013-12-02",
+        value: 1055.42
+    },
+    {
+        date: "2013-12-03",
+        value: 1096
+    },
+    {
+        date: "2013-12-04",
+        value: 1156.12
+    },
+    {
+        date: "2013-12-05",
+        value: 1154.36
+    },
+    {
+        date: "2013-12-06",
+        value: 1042.38
+    },
+    {
+        date: "2013-12-07",
+        value: 854.64
+    },
+    {
+        date: "2013-12-08",
+        value: 802.51
+    },
+    {
+        date: "2013-12-09",
+        value: 921.93
+    },
+    {
+        date: "2013-12-10",
+        value: 997.23
+    },
+    {
+        date: "2013-12-11",
+        value: 1001.58
+    },
+    {
+        date: "2013-12-12",
+        value: 901.94
+    },
+    {
+        date: "2013-12-13",
+        value: 941.79
+    },
+    {
+        date: "2013-12-14",
+        value: 904.65
+    },
+    {
+        date: "2013-12-15",
+        value: 886.16
+    },
+    {
+        date: "2013-12-16",
+        value: 882.25
+    },
+    {
+        date: "2013-12-17",
+        value: 754.83
+    },
+    {
+        date: "2013-12-18",
+        value: 679.32
+    },
+    {
+        date: "2013-12-19",
+        value: 707.23
+    },
+    {
+        date: "2013-12-20",
+        value: 729.16
+    },
+    {
+        date: "2013-12-21",
+        value: 654.27
+    },
+    {
+        date: "2013-12-22",
+        value: 666.74
+    },
+    {
+        date: "2013-12-23",
+        value: 680.91
+    },
+    {
+        date: "2013-12-24",
+        value: 684.39
+    },
+    {
+        date: "2013-12-25",
+        value: 682.7
+    },
+    {
+        date: "2013-12-26",
+        value: 777.75
+    },
+    {
+        date: "2013-12-27",
+        value: 777.51
+    },
+    {
+        date: "2013-12-28",
+        value: 747.06
+    },
+    {
+        date: "2013-12-29",
+        value: 748.61
+    },
+    {
+        date: "2013-12-30",
+        value: 766.6
+    },
+    {
+        date: "2013-12-31",
+        value: 760.58
+    }
+];
+// -------------------------------Data manipulation--------
+const parse = d3.timeParse("%Y-%m-%d");
+const bisectDate = d3.bisector((d) => d.date).left;
+
+// data manipulation first
+data = data.map((datum) => {
+    datum.date = parse(datum.date);
+return datum;
+});
+// -------------------------------Data manipulation ends--------
+// Add X axis --> it is a date format
+var x = d3
+    .scaleTime()
+    .domain(
+        d3.extent(data, function (d) {
+            return d.date;
+        })
+    )
+    .range([0, width]);
+
+svg
+    .append("g")
+    .attr("class", "myXaxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+// Add Y axis
+var y = d3
+    .scaleLinear()
+    .domain([
+        0,
+        d3.max(data, function (d) {
+            return +d.value;
+        })
+    ])
+    .range([height, 0]);
+
+svg.append("g").call(d3.axisLeft(y));
+//Read the data
+// Now I can use this dataset:
+function line() {
+    d3.select("#cvg_svgC2").remove();
+    d3.select("#cvg_svgC3").remove();
+
+    // Add the line
+
+    svg2
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "none")
+        .attr("stroke-width", 2)
+        .attr(
+            "d",
+            d3
+                .line()
+                .x(function (d) {
+                    return x(d.date);
+                })
+                .y(function (d) {
+                    return y(d.value);
+                })
+        )
+        .style("stroke", "#0594fa");
+}
+
+function bar() {
+    // Bars
+    d3.select("#cvg_svgC2").selectAll("path").remove();
+    d3.select("#cvg_svgC4").selectAll("circle").remove();
+
+    svg3
+        .selectAll("mybar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) {
+            return x(d.date);
+        })
+        .attr("width", 1.5)
+        .attr("fill", "#fcba03")
+        // no bar at the beginning thus:
+        .attr("height", function (d) {
+            return height - y(0);
+        }) // always equal to 0
+        .attr("y", function (d) {
+            return y(0);
+        });
+
+    // Animation
+    svg3
+        .selectAll("rect")
+        .transition()
+        .duration(800)
+        .attr("y", function (d) {
+            return y(d.value);
+        })
+        .attr("height", function (d) {
+            return height - y(d.value);
+        })
+        .delay(function (d, i) {
+            console.log(i);
+            return i * 5;
+        });
+}
+
+function scatter() {
+    // scatter plots
+
+    d3.select("#cvg_svgC2").remove();
+    d3.select("#cvg_svgC3").remove();
+
+    svg4
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "none")
+        .attr("stroke-width", 2)
+        .attr(
+            "d",
+            d3
+                .line()
+                .x(function (d) {
+                    return x(d.date);
+                })
+                .y(function (d) {
+                    return y(d.value);
+                })
+        )
+        .style("stroke", "#0594fa");
+
+    svg4
+        .select(".myXaxis")
+        .transition()
+        .duration(800)
+        .attr("opacity", "1")
+        .call(d3.axisBottom(x));
+
+    svg4
+        .selectAll("circle")
+        .transition()
+        .delay(function (d, i) {
+            return i * 5;
+        })
+        .duration(800)
+        .attr("cx", function (d) {
+            return x(d.date);
+        })
+        .attr("cy", function (d) {
+            return y(d.value);
+        })
+        .style("fill", "#0594fa");
+}
+
+d3.select("select").on("change", function (d) {
+    var selected = d3.select("#d3-dropdown").node().value;
+
+    if (selected == "line") {
+        line();
+    } else if (selected == "bar") {
+        bar();
+    } else if (selected == "scatter") {
+        scatter();
+    }
+});
