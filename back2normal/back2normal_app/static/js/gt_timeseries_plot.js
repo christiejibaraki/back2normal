2885f56bddacf9429a8a3630f1893fc532af7993
Christine Ibaraki <>
2021-03-16 13:53:48 -0500

    change plot width

--- back2normal/back2normal_app/static/js/gt_timeseries_plot.js
+++ back2normal/back2normal_app/static/js/gt_timeseries_plot.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
+    width = 800 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // parse the date and time
