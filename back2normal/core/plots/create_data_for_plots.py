b0cae5b2b5845a3623306b6df294059ec03e9f68
ColeVFrank <cole.v.frank@gmail.com>
2021-03-15 20:30:18 -0400

    created js file with vaccination data in list of dicts format

--- back2normal/core/plots/create_data_for_plots.py
+++ back2normal/core/plots/create_data_for_plots.py
@@ -13,8 +13,14 @@ def get_demographic_data(output_file):
         for cat in census_df.columns:
             demographic_data.append({"ZIPCODE": zipc, "CATEGORY": cat, 
                                      "VALUE": census_df[cat][i]})
-    basic_io.write_dict_to_json(output_file, demographic_data)
+    #basic_io.write_dict_to_json(output_file, demographic_data)
 
-get_demographic_data("demographic_data.js")
+#get_demographic_data("demographic_data_maybe.js")
 
 
+def get_vaccine_data(output_file):
+    db = dbclient.DBClient()
+    query = f"select * from {build_db.VACC_TBL}"
+    vacc_df = pd.read_sql_query(query, db.conn)
+    vacc_records = vacc_df.to_dict(orient='records')
+    basic_io.write_dict_to_json(output_file, vacc_records)
