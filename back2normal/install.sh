84156da0ebad032c98e744e9051c77cf7d4d953a
Christine Ibaraki <>
2021-03-16 03:30:27 -0500

    add env to install.sh

--- back2normal/install.sh
+++ back2normal/install.sh
@@ -23,3 +23,29 @@ PLATFORM=$($PYTHON -c 'import platform; print(platform.system())')
 
 echo -e "2. Checking OS Platform..."
 echo -e "\t--OS=Platform=$PLATFORM"
+
+# 3. Create Virtual environment
+echo -e "3. Creating new virtual environment..."
+
+# Remove the env directory if it exists
+if [[ -d env ]]; then
+    echo -e "\t--Virtual Environment already exists. Deleting old one now."
+    rm -rf env
+fi
+
+$PYTHON -m venv env
+if [[ ! -d env ]]; then
+    echo -e "\t--Could not create virutal environment...Please make sure venv is installed"
+    exit 1
+fi
+
+# 4. Install Requirements
+
+echo -e "4. Installing Requirements..."
+if [[ ! -e "requirements.txt" ]]; then
+    echo -e "\t--Need to requirements.txt to install packages."
+    exit 1
+fi
+
+source env/bin/activate
+pip install -r requirements.txt
