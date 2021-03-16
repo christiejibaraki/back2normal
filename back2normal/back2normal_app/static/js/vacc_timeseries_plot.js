8fea1d8ee152cda1d7bf6af09879ed98ac3d803b
Christine Ibaraki <>
2021-03-15 23:38:50 -0500

    code reformat for vacc series js

--- back2normal/back2normal_app/static/js/vacc_timeseries_plot.js
+++ back2normal/back2normal_app/static/js/vacc_timeseries_plot.js
@@ -9,30 +9,30 @@ var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
 
 // append the svg object to the body of the page
 var svg = d3.select("#vaccTime")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+        "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
 var data = vacc_records
 //var data = [{"index": 1345, "ZIPCODE": "60629", "STD_DATE": "2021-02-19 00:00:00", "total_doses_daily": 486, "total_doses_cumulative": 10606, "vaccine_series_completed_daily": 180, "vaccine_series_completed_percent_population": 0.018000000000000002, "population": 110029, "AVG7DAY_total_doses_daily": 357.0, "AVG7DAY_vaccine_series_completed_daily": 98.85714285714286},
 //            {"index": 812, "ZIPCODE": "60617", "STD_DATE": "2021-02-19 00:00:00", "total_doses_daily": 403, "total_doses_cumulative": 8982, "vaccine_series_completed_daily": 252, "vaccine_series_completed_percent_population": 0.028999999999999998, "population": 83553, "AVG7DAY_total_doses_daily": 280.7142857142857, "AVG7DAY_vaccine_series_completed_daily": 136.42857142857142},
 //            {"index": 3235, "ZIPCODE": "60661", "STD_DATE": "2021-02-19 00:00:00", "total_doses_daily": 46, "total_doses_cumulative": 2389, "vaccine_series_completed_daily": 22, "vaccine_series_completed_percent_population": 0.083, "population": 10354, "AVG7DAY_total_doses_daily": 40.57142857142857, "AVG7DAY_vaccine_series_completed_daily": 16.857142857142858}]
-  //{'year': 1884, 'sex': 'F', 'name': 'John', 'n': 50, 'prop': 0.00651}
-  //{"index": 2293, "ZIPCODE": "60654", "STD_DATE": "2020-12-15 00:00:00", 
-  //"total_doses_daily": 0, "total_doses_cumulative": 0,
-  //"vaccine_series_completed_daily": 0, "vaccine_series_completed_percent_population": 0.0,
-  //"population": 20022, "AVG7DAY_total_doses_daily": NaN,
-  //"AVG7DAY_vaccine_series_completed_daily": NaN}
+//{'year': 1884, 'sex': 'F', 'name': 'John', 'n': 50, 'prop': 0.00651}
+//{"index": 2293, "ZIPCODE": "60654", "STD_DATE": "2020-12-15 00:00:00",
+//"total_doses_daily": 0, "total_doses_cumulative": 0,
+//"vaccine_series_completed_daily": 0, "vaccine_series_completed_percent_population": 0.0,
+//"population": 20022, "AVG7DAY_total_doses_daily": NaN,
+//"AVG7DAY_vaccine_series_completed_daily": NaN}
 
-  //2021-02-19 00:00:00
-  //%Y-%m-%d %H:%M:%S
+//2021-02-19 00:00:00
+//%Y-%m-%d %H:%M:%S
 // handle the date
-function datehandle(dataset){
-    dataset.forEach(function(d){
+function datehandle(dataset) {
+    dataset.forEach(function (d) {
         // console.log(d.STD_DATE, typeof(d.STD_DATE))
         d.STD_DATE = parseDate(d.STD_DATE);
         // console.log(d.STD_DATE, typeof(d.STD_DATE))
@@ -58,56 +58,67 @@ function datasetVaccLineChosen(group) {
 data = datasetVaccLineChosen(group)
 
 // group the data: I want to draw one line per group
-function datafunc(d){
-var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
-  .key(function(d) { return d.ZIPCODE;})
-  .entries(data);
+function datafunc(d) {
+    var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+        .key(function (d) {
+            return d.ZIPCODE;
+        })
+        .entries(data);
 
-  // Add X axis --> it is a date format
-var xScale = d3.scaleTime()
-  .domain(d3.extent(data, function(d) { return d.STD_DATE; }))
-  .range([ 0, width ]);
+    // Add X axis --> it is a date format
+    var xScale = d3.scaleTime()
+        .domain(d3.extent(data, function (d) {
+            return d.STD_DATE;
+        }))
+        .range([0, width]);
 
-var xaxis = d3.axisBottom()
-  .ticks(d3.timeDay.every(15))
-  .tickFormat(d3.timeFormat('%b %d'))
-  .scale(xScale);
+    var xaxis = d3.axisBottom()
+        .ticks(d3.timeDay.every(15))
+        .tickFormat(d3.timeFormat('%b %d'))
+        .scale(xScale);
 
 
-  svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-    .call(xaxis);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xaxis);
 
-  // Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, d3.max(data, function(d) { return +d.AVG7DAY_total_doses_daily; })])
-  .range([ height, 0 ]);
-svg.append("g")
-  .call(d3.axisLeft(y));
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) {
+            return +d.AVG7DAY_total_doses_daily;
+        })])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
-  // color palette
-var res = sumstat.map(function(d){ return d.key }) // list of group names
-var color = d3.scaleOrdinal()
-  .domain(res)
-  .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+    // color palette
+    var res = sumstat.map(function (d) {
+        return d.key
+    }) // list of group names
+    var color = d3.scaleOrdinal()
+        .domain(res)
+        .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'])
 
-  // Draw the line
-svg.selectAll(".line")
-    .data(sumstat)
-    .enter()
-    .append("path")
-      .attr("fill", "none")
-      .attr("stroke", function(d){ return color(d.key) })
-      .attr("stroke-width", 1.5)
-      .attr("d", function(d){
-        return d3.line()
-            .x(function(d) {
-                return xScale(d.STD_DATE);
-            })
-          // .xScale(function(d) { return xScale(d.STD_DATE); })
-          .y(function(d) { return y(+d.AVG7DAY_total_doses_daily); })
-          (d.values)
-      })
+    // Draw the line
+    svg.selectAll(".line")
+        .data(sumstat)
+        .enter()
+        .append("path")
+        .attr("fill", "none")
+        .attr("stroke", function (d) {
+            return color(d.key)
+        })
+        .attr("stroke-width", 1.5)
+        .attr("d", function (d) {
+            return d3.line()
+                .x(function (d) {
+                    return xScale(d.STD_DATE);
+                })
+                .y(function (d) {
+                    return y(+d.AVG7DAY_total_doses_daily);
+                })
+                (d.values)
+        })
 }
 
 
