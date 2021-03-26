b3bdebf0f619fb09ef973e043aa76fce5e97a26b
Christine Ibaraki <>
2021-03-26 11:37:38 -0400

    refactor project

--- back2normal/back2normal_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Back2NormalAppConfig(AppConfig):
-    name = 'back2normal_app'
--- back2normal/back2normal_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def home(request):
-    # plot = bokeh_test.get_coles_plot()
-    # script, div = components(plot)
-    # return render(request, 'home.html', {'script': script, 'div': div})
-    return render(request, 'home.html', {})
